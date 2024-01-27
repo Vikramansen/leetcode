317dc262ea1f8d5447aacd3fc2b4eba126ec9cd9
Vikraman <Vikramansen@gmail.com>
2024-01-26 19:03:38 -0800

    v2

---
+++ palindrome_permutation.py
@@ -0,0 +1,45 @@
+import unittest
+
+
+def has_palindrome_permutation(a):
+
+    unpaired = set()
+    for char in a:
+        if char in unpaired:
+            unpaired.remove(char)
+        else:
+            unpaired.add(char)
+    return len(unpaired) <= 1
+
+# Complexity
+# O(n) time, and O(n) space.
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcbcd')
+        self.assertTrue(result)
+
+    def test_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabccbdd')
+        self.assertTrue(result)
+
+    def test_no_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcd')
+        self.assertFalse(result)
+
+    def test_no_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabbcd')
+        self.assertFalse(result)
+
+    def test_empty_string(self):
+        result = has_palindrome_permutation('')
+        self.assertTrue(result)
+
+    def test_one_character_string(self):
+        result = has_palindrome_permutation('a')
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
