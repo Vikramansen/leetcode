f81a177db8519df2a76d1b38451b0e7b03f67bde
Vikraman <Vikramansen@gmail.com>
2024-01-29 21:57:35 -0800

    v4.1

---
+++ rotate_array.py
@@ -0,0 +1,28 @@
+def rotate(nums, k):
+        k = k % len(nums)
+        l, r = 0, len(nums)-1
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l +=1
+            r -=1
+
+        l, r = 0, k -1
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l +=1
+            r -=1
+
+        l, r = k, len(nums)-1
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l +=1
+            r -=1
+        
+        return nums
+    
+def main():
+    nums = [1,2,3,4,5,6,7]
+    k = 3
+    print(rotate(nums, k))
+
+main()
