fa9eeda970b7f0603d55afe0fb0e2f414a185fcd
Vikraman <Vikramansen@gmail.com>
2024-02-13 15:34:51 -0800

    v11

---
+++ Misc/min_temp.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temp):
+        stack = []
+        res = [0] * len(temp)
+        for i in range(len(temp)):
+            while stack and temp[i] > temp[stack[-1]]:
+                last = stack.pop()
+                res[last] = i - last
+            stack.append(i)
+        return res
+
+def main():
+    a = [1,2,1,2,3,4,5]
+    print(Solution().dailyTemperatures(a))
+
+main()
